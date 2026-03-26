e90ad75cba539b6686886bc85475f128b604fbd3
Fiona D'Mello <40391218+FionaDmello@users.noreply.github.com>
2026-03-26 16:24:22 +0100

    toggle button styling fix

--- src/features/Toggle/Toggle.tsx
+++ src/features/Toggle/Toggle.tsx
@@ -3,7 +3,7 @@ import Button from "@features/base/components/Button"
 const Toggle = () => {
   return (
     <div id="toggle-container" className="flex gap-4 rounded md:inline-block">
-      <Button type="monthly" className="flex-1 text-base leading-6 text-neutral-600" text="Monthly" />
+      <Button type="monthly" className="flex-1 text-base gap-1.5 px-4 py-2.5 leading-6 text-neutral-600" text="Monthly" />
       <Button
         type="annual"
         className="flex-1 gap-1.5 px-4 py-2.5 border-0.5 border-neutral-200 rounded shadow"
